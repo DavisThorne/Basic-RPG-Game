6aaec56d62a450ca75ee8d804c06845be00654f4
Davis <61391491+dskmgmt@users.noreply.github.com>
2022-12-14 22:31:02 +0000

    Upddated CreateNewGame.cs

--- CreateNewGame.cs
+++ CreateNewGame.cs
@@ -1,16 +1,18 @@
 ﻿using System.IO;
 using System;
 using System.Collections.Generic;
+using System.Text;
 using Newtonsoft.Json;
+using static System.IO.FileMode;
 
 namespace Basic_RPG_Game;
 
 class CharacterInfo
 {
-    public string name {get; set;}
-    public string characterClass {get; set;}
-    public int level {get; set;}
-    public List<string> stats {get; set;}
+    public string name { get; set; }
+    public string characterClass { get; set; }
+    public int level { get; set; }
+    public List<string> stats { get; set; }
 }
 
 public static class CreateNewGame
@@ -22,8 +24,8 @@ public static class CreateNewGame
         CreateFile(name);
         return name;
     }
-    
-    private static void CreateFile(string? name)
+
+    private static string CreateFile(string? name)
     {
         var path = Environment.ExpandEnvironmentVariables("%USERPROFILE%\\OneDrive\\Documents\\RPGGame\\");
         var timeDate = DateTime.Now;
@@ -31,44 +33,46 @@ public static class CreateNewGame
         {
             Directory.CreateDirectory(path);
         }
+
         var fileName = $"{name}{timeDate:dd-MM-yyyy-HH-mm-ss}.json";
         var fullPath = Path.Combine(path, fileName);
         Console.WriteLine(fullPath);
-        File.Create(fullPath);
-        DataCollection(name);
+        File.Create(fullPath).Dispose();
+        DataCollection(name, fullPath);
+        return fullPath;
     }
 
-    private static void DataCollection(String? name)
+    private static void DataCollection(string? name, string fullPath)
     {
         Console.WriteLine("Please Chose a Class. \n 1. Warrior \n 2. Mage \n 3. Archer");
         var classChoice = Console.ReadLine();
         string characterClass;
         var level = 1;
-        
+
         switch (classChoice)
         {
             case "1":
                 Console.WriteLine("You have chosen Warrior");
                 characterClass = "Warrior";
-                JsonParser(characterClass, level, name);
+                JsonParser(characterClass, level, name, fullPath);
                 break;
             case "2":
                 Console.WriteLine("You have chosen Mage");
                 characterClass = "Mage";
-                JsonParser(characterClass, level, name);
+                JsonParser(characterClass, level, name, fullPath);
                 break;
             case "3":
                 Console.WriteLine("You have chosen Archer");
                 characterClass = "Archer";
-                JsonParser(characterClass, level, name);
+                JsonParser(characterClass, level, name, fullPath);
                 break;
             default:
                 Console.WriteLine("Please enter a valid number");
                 break;
         }
     }
-    
-    private static void JsonParser(string characterClass, int level, string? name)
+
+    private static void JsonParser(string characterClass, int level, string? name, string fullPath)
     {
         var stats = new List<string>();
         switch (characterClass)
@@ -100,16 +104,21 @@ public static class CreateNewGame
             level = level,
             stats = stats
         };
-        
-        string json = JsonConvert.SerializeObject(characterInfo);
-        Console.WriteLine(json);
+
+        string jsonString = JsonConvert.SerializeObject(characterInfo, Formatting.Indented);
+        using (FileStream fs = File.Open(fullPath, FileMode.Open))
+        {
+            Byte[] json = new UTF8Encoding(true).GetBytes(jsonString);
+            fs.Write(json);
+        }
+
+        Console.WriteLine(jsonString);
     }
 
-    
+
     public static void Main()
     {
         Console.WriteLine("Welcome to the world of RPG!");
         NameInput();
-
     }
 }
